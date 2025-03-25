b43e22e4640d540b0c101f466a408f7172068299
AAllain04 <arnaultallain@gmail.com>
2025-03-25 12:03:26 +0100

    Déplacement du color-mode dans une view

--- src/views/footer.php
+++ src/views/footer.php
@@ -1,4 +1,4 @@
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-        <script src="../../public/js/color-modes.js"
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous" async defer></script>
+        <script src="../../public/js/color-modes.js" async defer></script>
         </body>
 </html>
