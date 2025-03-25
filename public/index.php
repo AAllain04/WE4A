f484d9da4bedbacba815fb2d1db5938255f7f63f
AAllain04 <arnaultallain@gmail.com>
2025-03-25 13:20:58 +0100

    Suppression du color mode, ajout d'images

--- public/index.php
+++ public/index.php
@@ -1,74 +1,83 @@
 <?php include('../src/views/header.php');
-include('../src/views/color-mode.php')?>
-
+?>
 
     <!-- Formulaire connexion -->
-    <main class="form-signin w-100 m-auto">
-        <form action="../src/moodle.php" method="get">
-            <h1 class="h3 mb-3 fw-normal">Please connect</h1>
+<body>
+        <main class="form-signin w-100 m-auto">
+            <form action="../src/moodle.php" method="get">
+                <h1 class="h3 mb-3 fw-normal">Please connect</h1>
 
-            <div class="form-floating position-relative">
-                <input
-                        type="email"
-                        class="form-control"
-                        id="floatingInput"
-                        placeholder="name@example.com"
-                        required
-                >
-                <label for="floatingInput">Email address</label>
-            </div>
+                <div class="form-floating position-relative">
+                    <input
+                            type="email"
+                            class="form-control"
+                            id="floatingInput"
+                            placeholder="name@example.com"
+                            required
+                    >
+                    <label for="floatingInput">Email address</label>
+                </div>
 
-            <div class="form-floating position-relative">
-                <input
-                        type="password"
-                        class="form-control"
-                        id="floatingPassword"
-                        placeholder="Password"
-                        required
-                        minlength="5"
-                >
-                <label for="floatingPassword">Password</label>
+                <div class="form-floating position-relative">
+                    <input
+                            type="password"
+                            class="form-control"
+                            id="floatingPassword"
+                            placeholder="Password"
+                            required
+                            minlength="5"
+                    >
+                    <label for="floatingPassword">Password</label>
 
-                <!-- Bouton pour afficher/masquer le mot de passe -->
+                    <!-- Bouton pour afficher/masquer le mot de passe -->
+                    <button
+                            type="button"
+                            class="btn btn-outline-secondary position-absolute top-50 end-0 translate-middle-y me-2 border-0"
+                            aria-label="Toggle password visibility"
+                            onclick="
+                            const passwordInput = document.getElementById('floatingPassword');
+                            this.innerHTML = passwordInput.type === 'password' ?
+                                '<i class=\'bi bi-eye\'></i>' :
+                                '<i class=\'bi bi-eye-slash\'></i>';
+                            passwordInput.type = passwordInput.type === 'password' ? 'text' : 'password';
+                        "
+                    >
+                        <i class="bi bi-eye-slash"></i>
+                    </button>
+                </div>
+
+                <div class="form-check text-start my-3">
+
+                    <!-- A configurer lors de l'ajout de Symfony -->
+                    <input
+                            class="form-check-input"
+                            type="checkbox"
+                            value="remember-me"
+                            id="flexCheckDefault"
+                    >
+                    <label class="form-check-label" for="flexCheckDefault">
+                        Remember me
+                    </label>
+                </div>
+
                 <button
-                        type="button"
-                        class="btn btn-outline-secondary position-absolute top-50 end-0 translate-middle-y me-2 border-0"
-                        aria-label="Toggle password visibility"
-                        onclick="
-                        const passwordInput = document.getElementById('floatingPassword');
-                        this.innerHTML = passwordInput.type === 'password' ?
-                            '<i class=\'bi bi-eye\'></i>' :
-                            '<i class=\'bi bi-eye-slash\'></i>';
-                        passwordInput.type = passwordInput.type === 'password' ? 'text' : 'password';
-                    "
+                        class="btn btn-primary w-100 py-2"
+                        type="submit"
                 >
-                    <i class="bi bi-eye-slash"></i>
+                    Sign in
                 </button>
-            </div>
 
-            <div class="form-check text-start my-3">
-
-                <!-- A configurer lors de l'ajout de Symfony -->
-                <input
-                        class="form-check-input"
-                        type="checkbox"
-                        value="remember-me"
-                        id="flexCheckDefault"
-                >
-                <label class="form-check-label" for="flexCheckDefault">
-                    Remember me
-                </label>
-            </div>
-
-            <button
-                    class="btn btn-primary w-100 py-2"
-                    type="submit"
-            >
-                Sign in
-            </button>
-
-            <p class="mt-5 mb-3 text-body-secondary">&copy; 2025–2025</p>
-        </form>
-    </main>
+                <p class="mt-5 mb-3 text-body-secondary">&copy; 2025–2025</p>
+            </form>
+        </main>
     <script src="./js/index.js" defer async></script>
+    <script>
+        document.addEventListener("DOMContentLoaded", function () {
+            // Appliquer le style height:100% à html et body
+            document.documentElement.setAttribute("style", "height: 100%;");
+            document.body.setAttribute("style", "height: 100%;");
+        });
+    </script>
+
+
 <?php include('../src/views/footer.php') ?>
--- src/moodle.php
+++ src/moodle.php
@@ -1,101 +1,87 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8">
-    <title>Moodle</title>
-    <link rel="stylesheet" href="../public/css/style.css">
-  </head>
-  <body>
-    <div class="top">
-      <div class="left">
-        <img src="logo_blanc.png">
-      </div>
-      <div class="mid">
-        <ul>
-          <li><a href="moodle.html">Tableau de bord</a></li>
-          <li><a href="moodle.html">Mes cours</a></li>
-          <li><a href="moodle.html">Sites UTBM</a></li>
-          <li><a href="moodle.html">Stages / S.E.E.</a></li>
-        </ul>
-      </div>
-      <div class="right">
-        <a href="moodle.html">Profil</a>
-      </div>
-    </div>
+<?php include('../src/views/header.php') ?>
 
+<?php include('../src/views/nav.php') ?>
+
+
+    <body>
     <div class="page">
+        <div class="contenu">
+            <h2>Mes cours</h2>
+            <h3>Vue d'ensemble des cours</h3>
 
-      <div class="contenu">
-        <h2>Mes cours</h2>
-        <h3>Vue d'ensemble des cours</h3>
+            <hr>
 
-        <hr>
+            <input type="text" placeholder="Rechercher">
 
-        <input type="text" placeholder="Rechercher">
-        
-        <input list="trier" placeholder="Trier">
-        <datalist id="trier">
-          <option value="Alphabétique">
-          <option value="Plus récent">
-          <option value="Plus ancien">
-        </datalist>
-        
-        <div class="clear"></div>
+            <input list="trier" placeholder="Trier">
+            <datalist id="trier">
+                <option value="Alphabétique">
+                <option value="Plus récent">
+                <option value="Plus ancien">
+            </datalist>
 
-        <div class="cours">
-          <img src="back1.png">
-          <a href="moodle.html">Cours 1</a>
-          <p>Descriptionzaevbrrvzqczdseee</p>
-        </div>
-        <div class="cours">
-          <img src="back2.png">
-          <a href="moodle.html">Cours 2</a>
-          <p>Descriptionezvktklzd,zqm</p>
-        </div>
-        <div class="cours">
-          <img src="back3.png">
-          <a href="moodle.html">Cours 3</a>
-          <p>Descriptionezvezvrdzqm</p>
-        </div>
+            <div class="clear"></div>
 
-        <div class="clear"></div>
+            <div class="cours">
+                <img src="back1.png">
+                <a href="moodle.html">Cours 1</a>
+                <p>Descriptionzaevbrrvzqczdseee</p>
+            </div>
+            <div class="cours">
+                <img src="back2.png">
+                <a href="moodle.html">Cours 2</a>
+                <p>Descriptionezvktklzd,zqm</p>
+            </div>
+            <div class="cours">
+                <img src="back3.png">
+                <a href="moodle.html">Cours 3</a>
+                <p>Descriptionezvezvrdzqm</p>
+            </div>
 
-        <div class="cours">
-          <img src="back2.png">
-          <a href="moodle.html">Cours 4</a>
-          <p>Descriptionzaevbrrvzqczdseee</p>
-        </div>
-        <div class="cours">
-          <img src="back3.png">
-          <a href="moodle.html">Cours 5</a>
-          <p>Descriptionezvktklzd,zqm</p>
-        </div>
-        <div class="cours">
-          <img src="back1.png">
-          <a href="moodle.html">Cours 6</a>
-          <p>Descriptionezvezvrdzqm</p>
-        </div>
+            <div class="clear"></div>
 
-        <div class="clear"></div>
+            <div class="cours">
+                <img src="back2.png">
+                <a href="moodle.html">Cours 4</a>
+                <p>Descriptionzaevbrrvzqczdseee</p>
+            </div>
+            <div class="cours">
+                <img src="back3.png">
+                <a href="moodle.html">Cours 5</a>
+                <p>Descriptionezvktklzd,zqm</p>
+            </div>
+            <div class="cours">
+                <img src="back1.png">
+                <a href="moodle.html">Cours 6</a>
+                <p>Descriptionezvezvrdzqm</p>
+            </div>
 
-        <div class="cours">
-          <img src="back3.png">
-          <a href="moodle.html">Cours 7</a>
-          <p>Descriptionzaevbrrvzqczdseee</p>
-        </div>
-        <div class="cours">
-          <img src="back1.png">
-          <a href="moodle.html">Cours 8</a>
-          <p>Descriptionezvktklzd,zqm</p>
-        </div>
-        <div class="cours">
-          <img src="back2.png">
-          <a href="moodle.html">Cours 9</a>
-          <p>Descriptionezvezvrdzqm</p>
-        </div>
+            <div class="clear"></div>
 
-      </div>
-    
+            <div class="cours">
+                <img src="back3.png">
+                <a href="moodle.html">Cours 7</a>
+                <p>Descriptionzaevbrrvzqczdseee</p>
+            </div>
+            <div class="cours">
+                <img src="back1.png">
+                <a href="moodle.html">Cours 8</a>
+                <p>Descriptionezvktklzd,zqm</p>
+            </div>
+            <div class="cours">
+                <img src="back2.png">
+                <a href="moodle.html">Cours 9</a>
+                <p>Descriptionezvezvrdzqm</p>
+            </div>
+        </div>
     </div>
-  </body>
-</html>
+
+    <script>
+        document.addEventListener("DOMContentLoaded", function () {
+            // Supprimer l'attribut style pour revenir aux styles par défaut
+            document.documentElement.removeAttribute("style");
+            document.body.removeAttribute("style");
+        });
+    </script>
+
+<?php include('../src/views/footer.php') ?>
